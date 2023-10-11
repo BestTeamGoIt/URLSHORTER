44cf21ebd40eabc43b1774d979ce59deb950e014
oleg-laizik <oleglaizik@gmail.com>
2023-10-11 19:04:07 +0200

    add authentication and refreshToken tests.

--- src/test/java/com/bestteam/urlshorter/AuthenticationControllerTest.java
+++ src/test/java/com/bestteam/urlshorter/AuthenticationControllerTest.java
@@ -1,5 +1,7 @@
 package com.bestteam.urlshorter;
 
+import com.bestteam.urlshorter.auth.AuthenticationRequest;
+import com.bestteam.urlshorter.auth.AuthenticationResponse;
 import com.bestteam.urlshorter.auth.RegistrationRequest;
 import com.bestteam.urlshorter.constants.Role;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -7,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -55,5 +58,84 @@ public class AuthenticationControllerTest extends SpringBootApplicationTest {
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isBadRequest())
                 .andDo(MockMvcResultHandlers.print());
+    }
+
+    @Test
+    void authenticateTest() throws Exception {
+        RegistrationRequest request = new RegistrationRequest();
+        request.setFirstname("Ivan");
+        request.setLastname("Argon");
+        request.setEmail("oletes@gmail.com");
+        request.setPassword("asfggdhgh2");
+        request.setRole(Role.USER);
+
+        mockMvc.perform(post("/api/v1/auth/registration")
+                        .content(objectMapper.writeValueAsString(request))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(jsonPath("$.access_token").isNotEmpty())
+                .andExpect(jsonPath("$.refresh_token").isNotEmpty());
+
+        AuthenticationRequest authenticationRequest = new  AuthenticationRequest();
+
+        authenticationRequest.setEmail("oletes@gmail.com");
+        authenticationRequest.setPassword("asfggdhgh2");
+
+        mockMvc.perform(post("/api/v1/auth/authenticate")
+                        .content(objectMapper.writeValueAsString(authenticationRequest))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(jsonPath("$.access_token").isNotEmpty())
+                .andExpect(jsonPath("$.refresh_token").isNotEmpty());
+    }
+
+    @Test
+    void refreshTokenTest() throws Exception {
+        // Registration
+        RegistrationRequest request = new RegistrationRequest();
+        request.setFirstname("Ivan");
+        request.setLastname("Argon");
+        request.setEmail("kofnanf@gmail.com");
+        request.setPassword("asfggdhgh2");
+        request.setRole(Role.USER);
+
+        mockMvc.perform(post("/api/v1/auth/registration")
+                        .content(objectMapper.writeValueAsString(request))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(jsonPath("$.access_token").isNotEmpty())
+                .andExpect(jsonPath("$.refresh_token").isNotEmpty());
+
+        // Authentication
+        AuthenticationRequest authenticationRequest = new  AuthenticationRequest();
+
+        authenticationRequest.setEmail("kofnanf@gmail.com");
+        authenticationRequest.setPassword("asfggdhgh2");
+
+        MvcResult mvcResult = mockMvc.perform(post("/api/v1/auth/authenticate")
+                        .content(objectMapper.writeValueAsString(authenticationRequest))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(jsonPath("$.access_token").isNotEmpty())
+                .andExpect(jsonPath("$.refresh_token").isNotEmpty())
+                .andReturn();
+        String contentAsString = mvcResult.getResponse().getContentAsString();
+        String refreshToken;
+        AuthenticationResponse authenticationResponse = objectMapper.readValue(contentAsString, AuthenticationResponse.class);
+        refreshToken = authenticationResponse.getRefreshToken();
+        // RefreshToken
+
+
+        mockMvc.perform(post("/api/v1/auth/refresh-token")
+                        .header("Authorization", "Bearer " + refreshToken)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(jsonPath("$.access_token").isNotEmpty())
+                .andExpect(jsonPath("$.refresh_token").isNotEmpty());
     }
 }
