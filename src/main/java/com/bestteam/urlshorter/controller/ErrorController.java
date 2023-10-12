8763e395ba6342f0e6c902ca932556d2c96094ed
AlexandrMateush <128997943+AlexandrMateush@users.noreply.github.com>
2023-10-12 13:40:42 +0300

    controllersV2

--- src/main/java/com/bestteam/urlshorter/controller/ErrorController.java
+++ src/main/java/com/bestteam/urlshorter/controller/ErrorController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class ErrorController implements org.springframework.boot.web.servlet.error.ErrorController {
 
-
     @RequestMapping("/error")
     public String handleError(HttpServletRequest request) {
         Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
