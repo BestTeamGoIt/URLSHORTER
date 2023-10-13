1e957d8f46957e3e14ea7ec26f3d9d2fdd75f5db
oleg-laizik <oleglaizik@gmail.com>
2023-10-13 17:32:58 +0200

    add UUid too authentication and refreshToken tests.

--- src/test/java/com/bestteam/urlshorter/AuthenticationControllerTest.java
+++ src/test/java/com/bestteam/urlshorter/AuthenticationControllerTest.java
@@ -12,6 +12,8 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
+import java.util.UUID;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -29,8 +31,9 @@ public class AuthenticationControllerTest extends SpringBootApplicationTest {
     @Test
     void registrationTest() throws Exception {
         RegistrationRequest request = new RegistrationRequest();
-        request.setUsername("Ivan1");
-        request.setEmail("ivanargon@gmail.com");
+        String uid = UUID.randomUUID().toString();
+        request.setUsername("Ivan_" + uid);
+        request.setEmail("ivanargon+" + uid + "@gmail.com");
         request.setPassword("223123123gsgd");
         request.setRole(Role.USER);
 
@@ -46,8 +49,9 @@ public class AuthenticationControllerTest extends SpringBootApplicationTest {
     @Test
     void registrationTest_IfPasswordIsNullThenReturnBadRequest() throws Exception {
         RegistrationRequest request = new RegistrationRequest();
-        request.setUsername("Ivanargon");
-        request.setEmail("ivanargon@gmail.com");
+        String uid = UUID.randomUUID().toString();
+        request.setUsername("Ivanargon_" + uid);
+        request.setEmail("ivanargon_" + uid + "@gmail.com");
         request.setPassword(null);
         request.setRole(Role.USER);
 
@@ -61,8 +65,9 @@ public class AuthenticationControllerTest extends SpringBootApplicationTest {
     @Test
     void authenticateTest() throws Exception {
         RegistrationRequest request = new RegistrationRequest();
-        request.setUsername("Ivanoletes");
-        request.setEmail("oletes@gmail.com");
+        String uid = UUID.randomUUID().toString();
+        request.setUsername(uid);
+        request.setEmail(uid + "@gmail.com");
         request.setPassword("asfggdhgh2");
         request.setRole(Role.USER);
 
@@ -74,9 +79,9 @@ public class AuthenticationControllerTest extends SpringBootApplicationTest {
                 .andExpect(jsonPath("$.access_token").isNotEmpty())
                 .andExpect(jsonPath("$.refresh_token").isNotEmpty());
 
-        AuthenticationRequest authenticationRequest = new  AuthenticationRequest();
+        AuthenticationRequest authenticationRequest = new AuthenticationRequest();
 
-        authenticationRequest.setEmail("oletes@gmail.com");
+        authenticationRequest.setEmail(uid + "@gmail.com");
         authenticationRequest.setPassword("asfggdhgh2");
 
         mockMvc.perform(post("/api/v1/auth/authenticate")
@@ -90,10 +95,10 @@ public class AuthenticationControllerTest extends SpringBootApplicationTest {
 
     @Test
     void refreshTokenTest() throws Exception {
-        // Registration
         RegistrationRequest request = new RegistrationRequest();
-        request.setUsername("Ivankof");
-        request.setEmail("kofnanf@gmail.com");
+        String uid = UUID.randomUUID().toString();
+        request.setUsername(uid);
+        request.setEmail(uid + "@gmail.com");
         request.setPassword("asfggdhgh2");
         request.setRole(Role.USER);
 
@@ -105,10 +110,9 @@ public class AuthenticationControllerTest extends SpringBootApplicationTest {
                 .andExpect(jsonPath("$.access_token").isNotEmpty())
                 .andExpect(jsonPath("$.refresh_token").isNotEmpty());
 
-        // Authentication
-        AuthenticationRequest authenticationRequest = new  AuthenticationRequest();
+        AuthenticationRequest authenticationRequest = new AuthenticationRequest();
 
-        authenticationRequest.setEmail("kofnanf@gmail.com");
+        authenticationRequest.setEmail(uid + "@gmail.com");
         authenticationRequest.setPassword("asfggdhgh2");
 
         MvcResult mvcResult = mockMvc.perform(post("/api/v1/auth/authenticate")
@@ -124,7 +128,6 @@ public class AuthenticationControllerTest extends SpringBootApplicationTest {
         AuthenticationResponse authenticationResponse = objectMapper.readValue(contentAsString, AuthenticationResponse.class);
         refreshToken = authenticationResponse.getRefreshToken();
         // RefreshToken
-
 
         mockMvc.perform(post("/api/v1/auth/refresh-token")
                         .header("Authorization", "Bearer " + refreshToken)
